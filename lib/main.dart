6a55bbc8403f1fc3504e31319512a9c013a01f03
SuhAilMAnzoor <Sohailmanzoorsario@gmail.com>
2025-03-05 23:36:56 +0500

    My Todo Work App with Firebase Firestore

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:todo/firebase_options.dart';
+import 'package:todo/screens/todo_screen.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 196, 212, 244)
+        ),
+        useMaterial3: true,
+      ),
+      // Add My Todo Work App Class File
+      home: const TodoScreen(),
+    );
+  }
+}
